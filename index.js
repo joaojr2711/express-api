01dd14be7c6cc108fe370bc6c90bdb9fb8b21ac8
João Batista <joaobat1999@gmail.com>
2022-02-25 10:57:36 -0300

    teste vars

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 
+require('dotenv').config();
+
 const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Your Express API is up and running!');
 });
 
 app.get('/teste', (req, res) => {
-    res.send('Your Express API is up and running!', port);
+    res.send(`TESTE VARS: ${process.env.MONGO_URL}`);
 });
 
 app.listen(port, () => {
